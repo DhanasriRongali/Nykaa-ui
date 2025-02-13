4134ac8afedb8778b2b88c8b26ffc6b421f5067a
Rajesh.K <rajesh_kandregula@tecnics.com>
2025-02-13 18:53:31 +0530

    Enhance Sang These Beauty Steals component with additional styling and data structure updates for offers

--- src/app/home/sang-these-beauty-steals/sang-these-beauty-steals.component.ts
+++ src/app/home/sang-these-beauty-steals/sang-these-beauty-steals.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SangTheseBeautyStealsService } from '../../../services/sang-these-beauty-steals.service';
 import { CommonModule } from '@angular/common';
+import { BeautyStealImage } from '../../../types/beauty-steal.types';
 
 @Component({
   selector: 'app-sang-these-beauty-steals',
@@ -10,17 +11,26 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./sang-these-beauty-steals.component.css']
 })
 export class SangTheseBeautyStealsComponent implements OnInit {
-  titleImage: string | undefined;
-  displayImages: string[] = [];
+  titleImage: BeautyStealImage | undefined;
+  displayImages: BeautyStealImage[] = [];
 
   constructor(private beautyStealsService: SangTheseBeautyStealsService) { }
 
   ngOnInit(): void {
-    const beautyStealsData = this.beautyStealsService.getBeautyStealsData();
-    const titleItem = beautyStealsData.images.find(img => 'titleImage' in img);
-    if (titleItem && 'titleImage' in titleItem) {
-      this.titleImage = titleItem.titleImage;
+    const data = this.beautyStealsService.getBeautyStealsData();
+
+    if (data.images.length > 0) {
+      const titleImageData = data.images.find(img => img.titleImage);
+      this.titleImage = titleImageData;
+      // console.log('titleImage', this.titleImage);
+
+      this.displayImages = data.images.filter(img => img.displayImage)
+        .map(img => ({
+          id: img.id,
+          displayImage: img.displayImage!,
+          offerText: img.offerText!,
+          offerDescription: img.offerDescription!
+        }));
     }
-    this.displayImages = beautyStealsData.images.filter(img => 'displayImage' in img).map(img => img.displayImage!);
   }
 }
