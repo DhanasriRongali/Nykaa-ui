0570f5eeda57a67a0082e324f0ecc3da94790a8c
Rajesh.K <rajesh_kandregula@tecnics.com>
2025-02-13 14:40:26 +0530

    Add Flash Sales component with HTML, CSS, and basic functionality

---
+++ src/app/home/flash-sales/flash-sales.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FlashSalesService } from '../../../services/flashsale.services';
+import { FlashSaleImage } from '../../../types/flash-sales.types';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-flash-sales',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './flash-sales.component.html',
+  styleUrls: ['./flash-sales.component.css']
+})
+export class FlashSalesComponent implements OnInit {
+  banner: FlashSaleImage | undefined;
+  flashItems: FlashSaleImage[] = [];
+
+  constructor(private flashSalesService: FlashSalesService) { }
+
+  ngOnInit(): void {
+    const images = this.flashSalesService.getImages();
+    if (images && images.length > 0) {
+      this.banner = images[0];
+      this.flashItems = images.slice(1);
+    }
+  }
+}
