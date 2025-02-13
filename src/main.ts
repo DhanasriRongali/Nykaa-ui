236ebc99ecd106ff058fc068b8c7070eb702acdb
DhanasriRongali <dhanasri_rongali@tecnics.com>
2025-02-13 13:42:33 +0530

    Add OfferCardsComponent to main application template

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HeaderComponent } from './app/header/header.component';
+import { OfferCardsComponent } from './app/home/hard_to_resist_deals/hard_to_resist_deals.component';
 import { FooterComponent } from './app/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, OfferCardsComponent, FooterComponent],
   template: `
     <app-header></app-header>
-
+    <app-offer-cards></app-offer-cards>
     <app-footer></app-footer>
   `
 })
