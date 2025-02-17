e3f4a3c9dc2bfce69a38a72aa8f815b1d359d927
Rajesh4013 <rajesh_kandregula@tecnics.com>
2025-02-17 19:30:07 +0530

    Add category menu dropdown with hover interaction in header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,17 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '../../services/header.service';
 import { CommonModule } from '@angular/common';
 import { NavItems } from '../../types/header.types';
+import { CategoryMenuComponent } from '../nav/category/category.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CategoryMenuComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   mainNavItems: NavItems[] = [];
   categoryItems: NavItems[]  = [];
+  showCategoryMenu = false;
 
   constructor(private headerService: HeaderService) { }
 
