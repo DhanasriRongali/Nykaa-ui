35ee92b4e527bd272b6946a07996c53d9825cb62
Rajesh4013 <rajesh_kandregula@tecnics.com>
2025-02-18 13:42:06 +0530

    Update Angular dependencies and bootstrap configuration

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { HeaderComponent } from './app/header/header.component';
 import { FooterComponent } from './app/footer/footer.component';
+import { HomeComponent } from './app/home/home.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app-component/app-routing.module';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterModule,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    HttpClientModule
   ],
   template: `
     <app-header></app-header>
@@ -23,9 +31,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App { }
 
-bootstrapApplication(App, {
-  providers: [
-    provideHttpClient(),
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [],
+  bootstrap: [App]
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
