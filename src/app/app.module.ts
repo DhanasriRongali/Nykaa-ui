ae7685c8398e9e396f3f0f06af5b1dad51e8d6bf
Rajesh4013 <rajesh_kandregula@tecnics.com>
2025-02-19 15:04:00 +0530

    Add environment configuration files and remove default app module

--- src/app/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LoginModalComponent } from './components/login-modal/login-modal.component';
-
-@NgModule({
-  declarations: [
-    LoginModalComponent
-  ],
-})
-export class AppModule { } 
