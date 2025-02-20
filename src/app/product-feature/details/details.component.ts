e089a1ce1dfc00f989a71cafe0cc5bfb99b6ae0a
Rajesh4013 <rajesh_kandregula@tecnics.com>
2025-02-20 13:55:28 +0530

    Enhance product details component with HTML sanitization and input type safety

--- src/app/product-feature/details/details.component.ts
+++ src/app/product-feature/details/details.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-details',
@@ -9,12 +10,23 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-  @Input() description?: string;
+  @Input() description?: string | HTMLPreElement;
   @Input() stock?: number;
-  @Input() brandId?: string;
-  @Input() categoryId?: string;
-  @Input() subCategoryId?: string;
-  @Input() childSubCategoryId?: string;
+  @Input() brandId: string = '';
+  @Input() categoryId: string = '';
+  @Input() subCategoryId: string = '';
+  @Input() childSubCategoryId: string = '';
   @Input() originalPrice?: number;
-  @Input() createdAt!: string;
+  @Input() createdAt: string = '';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  get sanitizedDescription(): SafeHtml {
+    if (typeof this.description === 'string') {
+      return this.sanitizer.bypassSecurityTrustHtml(this.description);
+    } else if (this.description instanceof HTMLPreElement) {
+      return this.sanitizer.bypassSecurityTrustHtml(this.description.innerHTML);
+    }
+    return '';
+  }
 }
